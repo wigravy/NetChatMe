b8eb6a7bc7a33a1852c84c0ef5b5aea37f2a56fd
Shangaraev Ildar <wigravy@gmail.com>
2020-05-27 20:52:26 +0500

    ver 0.1a add fixed size for app window

--- src/source/Controller.java
+++ src/source/Controller.java
@@ -7,10 +7,15 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class Controller {
+
+
     @FXML
     private Button sendMessageButton;
 
@@ -34,10 +39,10 @@ public class Controller {
             keyEvent.consume();
         }
     }
-
+    //TODO: проработать покраску сообщений в разные цвета. Создать цвета которые будет приятно видеть и давать пользователю случайный уникальный цвет.
     private void sendMessage() {
         if (!writingTextArea.getText().trim().isEmpty()) {
-            messages.appendText(writingTextArea.getText().trim());
+            messages.appendText(getCurrentTime() + " " + getUserName() + ": " + writingTextArea.getText().trim());
             messages.appendText("\n");
             writingTextArea.clear();
             writingTextArea.requestFocus();
@@ -45,6 +50,18 @@ public class Controller {
             writingTextArea.clear();
             writingTextArea.requestFocus();
         }
+    }
+    //TODO: найти более оптимальный способ получения времени.
+    private String getCurrentTime() {
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        return dateFormat.format(date);
+    }
+
+    //TODO: сделать выбор имени пользователя для чата.
+    private String getUserName() {
+        return "wigravy";
     }
 
     @FXML
